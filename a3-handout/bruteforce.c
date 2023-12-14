864f19c197fcf2da32ef6703f9e7931ac14f529b
Nodifar <113943830+Nodifar@users.noreply.github.com>
2023-12-14 12:55:08 +0100

    Update bruteforce.c

--- a3-handout/bruteforce.c
+++ a3-handout/bruteforce.c
@@ -2,8 +2,15 @@
 #include "util.h"
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 
 int* knn(int k, int d, int n, const double *points, const double* query) {
-  assert(0);
+  int *closest = malloc(sizeof(int) * k);
+  for(int i = 0; i < k; i++) {
+    closest[i] = -1;
+  }
+  for(int i = 0; i < n; i++) {
+    insert_if_closer(k, d, points, closest, query, i);
+  }
+  return closest;
 }
-
